3f0e6b4c2e01243bec161ba6e6c8a663bc5ba3db
frank <frankbae10@gmail.com>
2020-04-24 00:11:44 +0000

    added header to components

---
+++ client/components/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <h2><i className="fas fa-dollar-sign"></i>Wicked Sales</h2>
+    );
+  }
+}
