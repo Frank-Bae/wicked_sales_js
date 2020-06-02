a6365c4edeb8415ea9e1ad11a07b8cfeb150db8a
frank <frankbae10@gmail.com>
2020-06-02 20:22:12 +0000

    added shipping address

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -17,11 +17,15 @@ export default class CheckoutForm extends React.Component {
         <form action="">
           <div className="form-group">
             <label htmlFor="name">Name</label>
-            <input type="text" className="form-control" id="name" placeHolder="John Doe"/>
+            <input type="text" className="form-control" id="name" placeholder="John Doe"/>
           </div>
           <div className="form-group">
             <label htmlFor="creditCard">Credit Card</label>
-            <input type="number" className="form-control" id="creditcard" placeHolder="0000 0000 0000 0000 0000"/>
+            <input type="number" className="form-control" id="creditcard" placeholder="0000-0000-0000-0000"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="shippingAddress">Shipping Address</label>
+            <textarea className="form-control" id="shippingAddress" rows="2" placeholder="9382 Brooklyn Blvd, New York, NY, 01923"></textarea>
           </div>
         </form>
       </div>
