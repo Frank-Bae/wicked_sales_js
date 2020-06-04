dec9e3dff868df6fb7bc93c6ebabb9949065ffe3
frank <frankbae10@gmail.com>
2020-06-04 22:29:01 +0000

    changed back to catalog

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -9,7 +9,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: { name: 'cart', params: {} },
+      view: { name: 'catalog', params: {} },
       cart: []
     };
     this.setView = this.setView.bind(this);
