7368f43de806a8331e117e90bb617746d1fa4698
frank <frankbae10@gmail.com>
2020-06-10 23:11:28 +0000

    added function connection handle button to the constructor

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
 export default class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true
+    };
+    this.handleButton = this.handleButton.bind(this);
+  }
+
+  handleButton() {
+    console.log('clicked');
+  }
 
   render() {
     return (
       <div id="modal">
         <div className="modalContent">
-          <h1></h1>
+          <h1>Welcome to Wicked Sales</h1>
 
-          <p>fasfsdfasfsfsdfsdfsdsffsd</p>
-          <button id="button">Close</button>
+          <p>This is a live demo site for the sole purpose of demonstartion. The products
+            displayed here are not avaibale for purchases. </p>
+          <button onClick={() => this.handleButton()} id="button">Close</button>
         </div>
       </div>
     );
