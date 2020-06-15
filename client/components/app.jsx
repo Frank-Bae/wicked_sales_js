a3e118912053a2282471d670eee5ba54f8663147
frank <frankbae10@gmail.com>
2020-06-15 21:47:23 +0000

    changed name to catalog

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -11,7 +11,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: { name: 'CheckoutForm', params: {} },
+      view: { name: 'catalog', params: {} },
       cart: []
     };
     this.setView = this.setView.bind(this);
