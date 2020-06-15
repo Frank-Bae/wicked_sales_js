c68c301da0f8c23378c9adda20dc3bae9953206d
frank <frankbae10@gmail.com>
2020-06-15 20:23:24 +0000

    made adjustments to height

--- client/components/banner.jsx
+++ client/components/banner.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 export default class Banner extends React.Component {
   render() {
     return (
-      <img className="quote" src="https://quotefancy.com/media/wallpaper/3840x2160/964358-Jonathan-Ive-Quote-Apple-s-goal-isn-t-to-make-money-Our-goal-is-to.jpg" alt=""/>
-
+      <img className="quote" src="https://quotefancy.com/media/wallpaper/3840x2160/964358-Jonathan-Ive-Quote-Apple-s-goal-isn-t-to-make-money-Our-goal-is-to.jpg" alt="" />
     );
   }
 }
