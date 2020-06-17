7ed5ecd04676a23ff312395bead19c8862d6bb28
frank <frankbae10@gmail.com>
2020-06-17 21:38:25 +0000

    completed the disclaimer

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -24,7 +24,7 @@ export default class Modal extends React.Component {
           <h1 className="bg-dark text-light text-center">Welcome to $Apples!</h1>
           <h4 className="red text-center"><strong>Disclaimer</strong></h4>
           <p className="text-center">The products and/or information contained in this website are for live demonstration purposes only. <span className="red">NONE OF THE PRODUCTS DISPLAYED ON WICKED SALES ARE FOR SALE.</span> No commercial transaction is intended in the creation and execution of the site. Please refrain from inputting any credit/debit card information or addresses in the site’s “checkout form.” </p>
-          <p className="text-center line">By accepting you acknowledge the merchandises displayed are not available for purchase </p>
+          <p className="text-center line">By clicking on the <span className="red">“I ACCEPT”</span> button below, you are acknowledging and accepting that Wicked Sale is not transacting any merchandise. </p>
           <div className="row">
             <div className="col text-center">
               <button className="btn btn-primary px-5" onClick={() => this.handleButton()} id="button">I ACCEPT</button>
