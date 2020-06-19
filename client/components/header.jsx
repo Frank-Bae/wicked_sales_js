b6e7cdc8fc0e3ea0e423b3cc2faa0a753625e5c2
frank <frankbae10@gmail.com>
2020-06-19 00:28:38 +0000

    add click icon to apple logo

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -6,7 +6,7 @@ function Header(props) {
   return (
     <div className=" bg-dark">
       <div className="container flex-header">
-        <h3 className="text-white py-2 pr-0">
+        <h3 className="text-white py-2 pr-0 hover" onClick={() => setView('catalog', {})}>
           <i className="fas fa-dollar-sign"></i>Apples
         </h3>
         <p className="hover" onClick={() => setView('cart', {})}>{`${cartItemCount} items`}<i onClick={() => setView('cart', {})} className="fas fa-shopping-cart fa-2x cart py-2 hover"></i></p>
